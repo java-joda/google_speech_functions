402bd1dd23b64cc1dc103643db6a0d245a373117
Vlad <vlad.pythoner@gmail.com>
2017-01-22 15:26:40 +0200

    *mp3 to *.raw conversion added.

--- gspeech.py
+++ gspeech.py
@@ -1,7 +1,7 @@
 import httplib2
 from googleapiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials
-
+from pydub import AudioSegment
 
 class SpeechRecognizer(object):
     """
@@ -12,7 +12,8 @@ class SpeechRecognizer(object):
         self.key_file = key_file  # path to json key file generated in Google Cloud Console
         self.access_point = None  # access point to get rights to interact with API
         self.audio_path = ''  # path to audio file (mp3)
-        self.raw_file_path = ''  # path to decoded from .mp3 .raw file
+        self._raw_file_path = ''  # path to decoded from .mp3 .raw file
+
 
     def getservice(self):
         """
@@ -29,6 +30,16 @@ class SpeechRecognizer(object):
             return
         self.access_point = service
 
+    def mp3_to_raw(self):
+        """
+        Convert file from mp3 to *raw
+        """
+        if not self.audio_path:
+            raise Exception(
+                'Nothing to convert! Specify a path to an *.mp3 file. Use SpeechRecognizer.audio_path parameter!')
+        self._raw_file_path = self.audio_path[:-3] + 'raw'
+        audiofile = AudioSegment.from_file(self.audio_path, "mp3")
+        audiofile.export(self._raw_file_path, format="raw")
 
 
 if __name__=="__main__":
